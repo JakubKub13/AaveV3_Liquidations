ffa05426b8af3e97dbba4385ca8c186b1d9865ed
JakubKub13 <jakubkubala3@gmail.com>
2022-11-28 13:31:12 +0100

    Setting up env

---
+++ contracts/flashLiquidations.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
+import {IFlashLoanSimpleReceiver} from "@aave/core-v3/contracts/flashloan/interfaces/IFlashLoanSimpleReceiver.sol";
+import {AaveProtocolDataProvider}
