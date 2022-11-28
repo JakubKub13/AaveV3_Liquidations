6806dc59959baa4a77a63746e859f873a9bfa38e
JakubKub13 <jakubkubala3@gmail.com>
2022-11-28 15:41:30 +0100

    adding state vars

--- contracts/flashLiquidations.sol
+++ contracts/flashLiquidations.sol
@@ -15,4 +15,30 @@ import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
 
 import "hardhat/console.sol";
 
-contract FlashLiquidations is FlashLoanSimpleReceiverBase, Ownable {}
+contract FlashLiquidations is FlashLoanSimpleReceiverBase, Ownable {
+    /// Parameters used for _liquidateAndSwap and final transfer of funds to owner
+    struct LiquidationParams {
+        address collateralAsset;
+        address borrowedAsset;
+        address user;
+        uint256 debtToCover;
+        uint24 poolFee1;
+        uint24 poolFee2;
+        address pathToken;
+        bool usePath;
+    }
+
+    ///Parameters used for liquidation and swap logic
+    struct LiquidationCallLocalVars {
+        uint256 initFlashBorrowedBalance;
+        uint256 diffFlashBorrowedBalance;
+        uint256 initCollateralBalance;
+        uint256 diffCollateralBalance;
+        uint256 flashLoanDebt;
+        uint256 soldAmount;
+        uint256 remainingTokens;
+        uint256 borrowedAssetLeftovers;
+    }
+
+    ISwapRouter public immutable swapRouter;
+}
