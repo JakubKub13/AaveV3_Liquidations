83354abcfc045906a9ecf4e2f1f0306f28a0b06a
JakubKub13 <jakubkubala3@gmail.com>
2022-11-28 13:40:52 +0100

    importing libraries

--- contracts/flashLiquidations.sol
+++ contracts/flashLiquidations.sol
@@ -3,4 +3,16 @@ pragma solidity 0.8.10;
 
 import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
 import {IFlashLoanSimpleReceiver} from "@aave/core-v3/contracts/flashloan/interfaces/IFlashLoanSimpleReceiver.sol";
-import {AaveProtocolDataProvider}
+import {AaveProtocolDataProvider} from "@aave/core-v3/contracts/misc/AaveProtocolDataProvider.sol";
+import {AaveOracle} from "@aave/core-v3/contracts/misc/AaveOracle.sol";
+import {FlashLoanSimpleReceiverBase} from "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
+import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
+import {IERC20} from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
+import {ISwapRouter} from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import {TransferHelper} from "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+
+import "hardhat/console.sol";
+
+contract FlashLiquidations is FlashLoanSimpleReceiverBase, Ownable {}
